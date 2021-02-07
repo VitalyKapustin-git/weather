655dbff66665ec278a4b32d06310d65589735342
VitalyKapustin-git <workgroupkap@gmail.com>
2021-02-08 00:00:24 +0300

    Init project

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  plugins: [
+    'jest',
+  ],
+  extends: [
+    'airbnb-base',
+    'prettier',
+    'prettier/prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'max-len': ['error', { ignoreComments: true }],
+  },
+};
