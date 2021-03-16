c15f42db68151f4a52acc32dc5af8c8b83fe174a
VitalyKapustin-git <workgroupkap@gmail.com>
2021-03-17 00:29:47 +0300

    Add test for geoposition

---
+++ tests/geoposition.test.js
@@ -0,0 +1,17 @@
+
+import { getUserGeo } from "../src/geoposition";
+
+beforeAll(() => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          city: "Иваново",
+        }),
+    })
+  );
+});
+
+test("if return right city when empty weather call", async () => {
+	expect(await getUserGeo()).toMatch('Иваново');
+});
